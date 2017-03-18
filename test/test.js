615fd7bb7c9615176baccda1b4238ed7b70cebdf
Sid Verma <mavidser@gmail.com>
2017-03-18 22:34:23 +0530

    Restructure and simplify the module

--- test/test.js
+++ test/test.js
@@ -1,48 +1,51 @@
 import test from 'ava';
 import es from '../';
 
-let sendPassingEmail = (t, emailService, providers) => {
-  return emailService.sendEmail({
-    from: ['frost.akemi@gmail.com'],
-    to: ['frost.akemi0@yopmail.com', 'frost.akemi1@yopmail.com'],
-    cc: ['frost.akemi2@yopmail.com', 'frost.akemi3@yopmail.com'],
-    bcc: ['frost.akemi4@yopmail.com', 'frost.akemi5@yopmail.com'],
-    subject: 'Hi',
-    body: '<b>Hi there!</b>',
-    isBodyHtml: true
-  }, providers);
+const passingEmail = {
+  from: ['frost.akemi@gmail.com'],
+  to: ['frost.akemi0@yopmail.com', 'frost.akemi1@yopmail.com'],
+  cc: ['frost.akemi2@yopmail.com', 'frost.akemi3@yopmail.com'],
+  bcc: ['frost.akemi4@yopmail.com', 'frost.akemi5@yopmail.com'],
+  subject: 'Hi',
+  body: '<b>Hi there!</b>',
+  isBodyHtml: true
 };
 
 test('SendGrid', async t => {
   const sendgrid = new es.SendGrid();
-  const emailService = new es.EmailService();
-  await sendPassingEmail(t, emailService, [sendgrid]);
+  await sendgrid.sendEmail(passingEmail);
+  t.pass();
 });
 
 test('Mailgun', async t => {
   const mailgun = new es.Mailgun();
-  const emailService = new es.EmailService();
-  await sendPassingEmail(t, emailService, [mailgun]);
+  await mailgun.sendEmail(passingEmail);
+  t.pass();
 });
 
 test('Mandrill', async t => {
   const mandrill = new es.Mandrill();
-  const emailService = new es.EmailService();
-  await sendPassingEmail(t, emailService, [mandrill]);
+  await mandrill.sendEmail(passingEmail);
+  t.pass();
 });
 
 test('SES', async t => {
   const ses = new es.SES();
-  const emailService = new es.EmailService();
-  await sendPassingEmail(t, emailService, [ses]);
+  await ses.sendEmail(passingEmail);
+  t.pass();
 });
 
 test('All Providers', async t => {
-  const emailServiceConfig = new es.EmailServiceConfig()
-                                   .addSendGrid()
-                                   .addMailgun()
-                                   .addMandrill()
-                                   .addSES();
-  const emailService = new es.EmailService(emailServiceConfig);
-  await sendPassingEmail(t, emailService);
+  const sendgrid = new es.SendGrid();
+  const mailgun = new es.Mailgun();
+  const mandrill = new es.Mandrill();
+  const ses = new es.SES();
+  const emailService = new es.EmailService([
+    sendgrid,
+    mailgun,
+    mandrill,
+    ses
+  ]);
+  await emailService.sendEmail(passingEmail);
+  t.pass();
 });
