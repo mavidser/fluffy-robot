bedb8492d3580e3e902148a2106148d4e47560b7
Sid Verma <mavidser@gmail.com>
2017-03-19 00:27:16 +0530

    Use config files for storing test data

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import es from '../';
+import config from 'config';
 
 const passingEmail = {
   from: 'frost.akemi@gmail.com',
@@ -16,72 +17,102 @@ test('EmailServiceProvider - No username', async t => {
 });
 
 test('EmailServiceProvider - No password', async t => {
-  const err = t.throws(() => new es.SendGrid('username'));
+  const err = t.throws(() => new es.SendGrid(config.SendGrid.username));
   t.is(err.message, 'Password is required for authentication.');
 });
 
 test('EmailServiceProvider - No argument while sending', async t => {
-  const sendgrid = new es.SendGrid('username', 'password');
+  const sendgrid = new es.SendGrid(config.SendGrid.username, config.SendGrid.password);
   const err = await t.throws(() => sendgrid.sendEmail());
   t.is(err.message, 'No argument found in the sendEmail function.');
 });
 
 test('EmailServiceProvider - No from', async t => {
-  const sendgrid = new es.SendGrid('username', 'password');
+  const sendgrid = new es.SendGrid(config.SendGrid.username, config.SendGrid.password);
   const err = await t.throws(() => sendgrid.sendEmail({
-    to: 'frost.akemi@gmail.com',
-    subject: 'Hi',
-    text: 'Hi there!'
+    to: config.SendGrid.to,
+    subject: config.SendGrid.subject,
+    text: config.SendGrid.text
   }));
   t.is(err.message, '\'from\' string is required in the details argument.');
 });
 
 test('EmailServiceProvider - No to', async t => {
-  const sendgrid = new es.SendGrid('username', 'password');
+  const sendgrid = new es.SendGrid(config.SendGrid.username, config.SendGrid.password);
   const err = await t.throws(() => sendgrid.sendEmail({
-    from: 'frost.akemi@gmail.com',
-    subject: 'Hi',
-    text: 'Hi there!'
+    from: config.SendGrid.from,
+    subject: config.SendGrid.subject,
+    text: config.SendGrid.text
   }));
   t.is(err.message, '\'to\' array/string is required in the details argument.');
 });
 
 test('SendGrid', async t => {
-  const sendgrid = new es.SendGrid('user','pass');
-  await sendgrid.sendEmail(passingEmail);
+  const sendgrid = new es.SendGrid(config.SendGrid.username, config.SendGrid.password);
+  await sendgrid.sendEmail({
+    to: config.SendGrid.to,
+    from: config.SendGrid.from,
+    subject: config.SendGrid.subject,
+    text: config.SendGrid.text,
+    html: config.SendGrid.html
+  });
   t.pass();
 });
 
 test('Mailgun', async t => {
-  const mailgun = new es.Mailgun('user', 'pass');
-  await mailgun.sendEmail(passingEmail);
+  const mailgun = new es.Mailgun(config.Mailgun.username, config.Mailgun.password);
+  await mailgun.sendEmail({
+    to: config.Mailgun.to,
+    from: config.Mailgun.from,
+    subject: config.Mailgun.subject,
+    text: config.Mailgun.text,
+    html: config.Mailgun.html
+  });
   t.pass();
 });
 
 test('Mandrill', async t => {
-  const mandrill = new es.Mandrill('user','pass');
-  await mandrill.sendEmail(passingEmail);
+  const mandrill = new es.Mandrill(config.Mandrill.username, config.Mandrill.password);
+  await mandrill.sendEmail({
+    to: config.Mandrill.to,
+    from: config.Mandrill.from,
+    subject: config.Mandrill.subject,
+    text: config.Mandrill.text,
+    html: config.Mandrill.html
+  });
   t.pass();
 });
 
 test('SES', async t => {
-  const ses = new es.SES('user','pass');
-  await ses.sendEmail(passingEmail);
+  const ses = new es.SES(config.SES.username, config.SES.password);
+  await ses.sendEmail({
+    to: config.SES.to,
+    from: config.SES.from,
+    subject: config.SES.subject,
+    text: config.SES.text,
+    html: config.SES.html
+  });
   t.pass();
 });
 
-test('All Providers', async t => {
-  const sendgrid = new es.SendGrid('user','pass');
-  const mailgun = new es.Mailgun('user','pass');
-  const mandrill = new es.Mandrill('user','pass');
-  const ses = new es.SES('user','pass');
+test('All Providers - Sendgrid', async t => {
+  const sendgrid = new es.SendGrid(config.SendGrid.username, config.SendGrid.password);
+  const mailgun = new es.Mailgun(config.Mailgun.username, config.Mailgun.password);
+  const mandrill = new es.Mandrill(config.Mandrill.username, config.Mandrill.password);
+  const ses = new es.SES(config.SES.username, config.SES.password);
   const emailService = new es.EmailService([
     sendgrid,
     mailgun,
     mandrill,
     ses
   ]);
-  await emailService.sendEmail(passingEmail);
+  await emailService.sendEmail({
+    to: config.SendGrid.to,
+    from: config.SendGrid.from,
+    subject: config.SendGrid.subject,
+    text: config.SendGrid.text,
+    html: config.SendGrid.html
+  });
   t.pass();
 });
 
